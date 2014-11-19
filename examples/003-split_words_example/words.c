fdc6ed78d16336b29d2c04a27c89ed4cc83efc68
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-11-19 15:03:04 +0100

    Example 003 now complete.
    Example 003 is from when I went through the quickstart guide at cgreen website.
    It contains two test cases for splitting a word.
    
    As of 2014-11-19 the quickstart guide can be found at
    http://cgreen.sourceforge.net/index.php/documentation/quickstart-guide under
    section **1.3 Five minutes doing TDD with Cgreen**.

--- examples/003-split_words_example/words.c
+++ examples/003-split_words_example/words.c
@@ -1,3 +1,16 @@
+#include <string.h>
+
 int split_words(char *sentence) {
-    return 0;
+    int i;
+    int count = 1;
+    int length = strlen(sentence);
+
+    for (i = 0; i < length; i++) {
+        if (sentence[i] == ' ') {
+            sentence[i] = '\0';
+            count++;
+        }
+    }
+
+    return count;
 }
