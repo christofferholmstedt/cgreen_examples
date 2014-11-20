50cdcd34f82e52c06678720e89bdc2713aa01cf0
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-11-20 14:58:30 +0100

    Added perl packages to provisioning script

--- scripts/01-install-required-packages.sh
+++ scripts/01-install-required-packages.sh
@@ -5,9 +5,13 @@
 # gcc compilation and some helper tools
 #############################################
 
+### Prevent Perl language issues
+sudo apt-get -y install language-pack-en language-pack-sv
+
 ### Git and Vim
 sudo apt-get -y install vim git
 
 ### Build tools
 sudo apt-get -y install build-essential doxygen \
-                        subversion cmake
+                        subversion cmake \
+                        perl-doc
